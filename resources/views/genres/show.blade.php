5ce38387e7a7b4818ef53bbad110cf6b3eb1bc4e
dT0210 <duthanh41025@gmail.com>
2023-12-20 15:31:07 +0700

    genre description added

--- resources/views/genres/show.blade.php
+++ resources/views/genres/show.blade.php
@@ -9,7 +9,14 @@
 
 <div class="w-full">
     <div class="w-[60%] mx-auto pt-5">
-        <div class="text-3xl font-serif font-bold text-cyan-900 border-b border-slate-300 pb-1">{{$genre->name}}</div>
+        <div class="border-b border-slate-300 pb-1">
+            <div class="text-3xl font-serif font-bold text-cyan-900 mb-2">
+                {{$genre->name}}
+            </div>
+            <div>
+                {{$genre->description}}
+            </div>
+        </div>
         <div class="mt-2">
             <x-book-row title="New releases" :books="$new" id="book-row-new"/>
             <x-book-row title="Highly rated" :books="$best" id="book-row-best"/>
